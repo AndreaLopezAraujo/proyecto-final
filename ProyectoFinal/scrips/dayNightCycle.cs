88d895f5eceb945866a41909bf5435f73bccf85a
AndreaLope <ac.lopez@uniandes.edu.co>
2021-05-18 13:16:54 -0500

    luz

---
+++ ProyectoFinal/scrips/dayNightCycle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayNightCycle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int rotationScale=50;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationScale*Time.deltaTime,0,0);
+    }
+}
